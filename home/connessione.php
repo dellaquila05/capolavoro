432afec545b806fdaff44a8324f5c0726a6e337e
FelixDeSouzaJefferson <felixdesouzajefferson@itis-molinari.eu>
2024-02-19 08:42:32 +0100

    modificato posizione file

--- home/connessione.php
+++ home/connessione.php
@@ -1,8 +1,7 @@
 <?php
-$path = '../config.json';
+$path = realpath('../config.json');
 $config_json = file_get_contents($path);
 $config = json_decode($config_json, true);
-
 $database_config = $config['database'];
 
 $host = $database_config['host'];
