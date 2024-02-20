0e56c77b1fe2c80a1572a69ca480794032f05625
MomoS216 <sametmohamed@itis-molinari.eu>
2024-02-20 14:25:53 +0100

    aggionamento

---
+++ service/servizi.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <title>Home</title>
+<style>
+    table img {
+        width: 40%; /* Imposta la larghezza al 100% della larghezza del contenitore */
+        height: auto; /* Imposta l'altezza automaticamente per mantenere l'aspetto originale */
+    }
+</style>
+</head>
+
+<body>
+
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="./home.html">HomeTech</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
+                aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            
+            <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                <span class="material-symbols-outlined">
+                    info
+                    </span>
+            </button>
+
+            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h1 class="modal-title fs-5" id="exampleModalLabel">HomeTech</h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            Benvenuto in HomeTech, il simulatore di gestione aziendale degli elettrodomestici!
+
+Inizia con un budget di 2000€ e gestisci il tuo impero settimana dopo settimana. Evita la bancarotta coprendo i costi fissi ogni 4 settimane. Affronta eventi imprevisti come guerre e furti, che influenzano le tue finanze.
+
+Ogni 48 settimane, paga le tasse e migliora la sicurezza con servizi come telecamere o allarmi. Acquista prodotti dal fornitore, soddisfa gli ordini e pianifica con attenzione. Sii strategico per trasformare il tuo impero in una storia di successo in HomeTech!
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+<div id="settimana">N settimana</div>
+        <div id="capitale">2000$</div>
+        </div>
+    </nav>
+    <br><br><br>
+
+
+    <div class="container mt-4">
+        <table class="table">
+            <tbody>
+                <tr>
+                    <td><a href="/management/magazzino.php">Magazzino <br><img src="/home/img/magazzino.png" alt=""><br></a></td>
+                    <td><a href="/management/fornitore.php">Fornitore <br><img src="/home/img/fornitore.png" alt=""><br></a></td>
+                
+                </tr>
+                <tr>
+                    <td><a href="/service/servizi.php" ><img src="/home/img/servizi.png" alt=""><br>Servizi</a></td>
+                    <td><a href="/service/resoconto.php"><img src="/home/img/resoconto.png" alt=""><br>Resoconto</a></td>
+ 
+                </tr>
+            </tbody>
+        </table>
+        <br><br>
+        <div class="row justify-content-end">
+            <div class="col-2">
+                <button type="submit" class="btn btn-primary">Avanti</button>
+            </div>
+        </div>
+    </div>
+    </div>
+
+
+
+
+
+</body>
+</html>
