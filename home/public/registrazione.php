bc1f7c771af6161f3766a9d65d4615c26918e6a6
FelixDeSouzaJefferson <felixdesouzajefferson@itis-molinari.eu>
2024-02-20 14:25:48 +0100

    aggiunto le insert nella tabella immagazina

--- home/public/registrazione.php
+++ home/public/registrazione.php
@@ -13,18 +13,35 @@ if ($connessione->query($sql1)) {
     $result = $connessione->query($sql2);
     if (mysqli_num_rows($result)) {
         $row = $result->fetch_array();
-        $val = $row['id'];
-        $sql3 = "INSERT INTO utente(username,password,n_settimana,idMagazzino)VALUES('$nome','$hashed_password',' 1 ',' $val ')";
+        $idMagazzino = $row['id'];
+        $sql3 = "INSERT INTO utente(username,password,n_settimana,idMagazzino)VALUES('$nome','$hashed_password',' 1 ',' $idMagazzino ')";
         if ($connessione->query($sql3)) {
             $sql4 = "SELECT * FROM magazzino ORDER BY id DESC LIMIT 1";
             $result = $connessione->query($sql4);
             if (mysqli_num_rows($result)) {
                 $row = $result->fetch_array();
-                $val = $row['id'];
-                $sql5 = "INSERT INTO costoFisso(nome,prezzo,idUtente)VALUES('Gas', 50, '$val'),('Luce', 450, '$val'),('Affitto', 1700, '$val');";
+                $idUtente = $row['id'];
+                $sql5 = "INSERT INTO costoFisso(nome,prezzo,idUtente)VALUES('Gas', 50, '$idUtente'),('Luce', 450, '$idUtente'),('Affitto', 1700, '$idUtente');";
                 if ($connessione->query($sql5)) {
-                    EchoMessage("Registrazione effettuata con successo", "../private/home.php");
-                    $_SESSION['loggato'] = true;
+                    $sql6 = "SELECT id FROM prodotto ORDER BY id";
+                    $result = $connessione->query($sql6);
+                    if (mysqli_num_rows($result)) {
+                        while ($idProdotto = $result->fetch_array()) {
+                            $sql7 = "INSERT INTO immagazzina(idMagazzino,idProdotto,quantitàPr) VALUES ('$idMagazzino', '$idProdotto', 0)";
+                            if (!$connessione->query($sql7)) {
+                                $interruttore = false;
+                                EchoMessage("Errore nella insert in immagazzina" . $sql7 . " . " . $connessione->error, "./registrazione.html");
+                                $connessione->close();
+                            }
+                        }
+                        if ($interruttore) {
+                            EchoMessage("Registrazione effettuata con successo", "../private/home.php");
+                            $_SESSION['loggato'] = true;
+                        }
+                    } else {
+                        EchoMessage("Errore nella select dai prodotti" . $sql6 . " . " . $connessione->error, "./registrazione.html");
+                        $connessione->close();
+                    }
                 } else {
                     EchoMessage("Errore con i costi fissi" . $sql5 . " . " . $connessione->error, "./registrazione.html");
                     $connessione->close();
