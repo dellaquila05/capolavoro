9a58df11533a538622262c6922ef961d32bace27
FelixDeSouzaJefferson <felixdesouzajefferson@itis-molinari.eu>
2024-02-21 13:03:52 +0100

    corretto la registrazione

--- home/public/registrazione.php
+++ home/public/registrazione.php
@@ -1,6 +1,88 @@
-<?php 
+<?php
 session_start();
-require_once("../connessione.php");
+require_once('../connessione.php');
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $nome = $connessione->real_escape_string($_POST['nomeUtente']); //recupero il nome con cui si vuole effettuare la registrazione
+    $password = $connessione->real_escape_string($_POST['password']); //recupero la password con cui si vuole effettuare la registrazione
+
+    try {
+        //verifico che il nome utente non sia già presente nel database
+        $sql = "SELECT username FROM utente WHERE username = '$nome'";
+        $result = $connessione->query($sql);
+        if (mysqli_num_rows($result)) {
+            $_SESSION['error'] = 1; //nome utente già presente nel database
+            $_SESSION['loggato'] = false; //blocco la registrazione
+            $connessione->close();
+        } else { //se il nome utente non è presente nel database vado avanti con la registrazione
+            if (strlen($password) < 6) { //se la password contiene meno di 6 caratteri blocco la registrazione
+                $_SESSION['error'] = 2; //password troppo corta
+                $_SESSION['loggato'] = false; //blocco la registrazione
+                $connessione->close();
+            } else { //se la password ha più di 5 caratteri vado avanti con la registrazione
+                $hashed_password = password_hash($password, PASSWORD_DEFAULT); //eseguo una funzione di hashing sulla password
+                //creo il magazzino del nuovo utente
+                $sql1 = "INSERT INTO magazzino(dimensione)VALUES(20)";
+                if ($connessione->query($sql1)) { //se non ci sono errori
+                    //recupero l'id del magazzino appena creato
+                    $sql2 = "SELECT id FROM magazzino ORDER BY id DESC LIMIT 1";
+                    $result = $connessione->query($sql2);
+                    if (mysqli_num_rows($result)) { //se non ci sono errori
+                        $idMagazzino = $result->fetch_array()['id'];
+                        //creo il nuovo utente
+                        $sql3 = "INSERT INTO utente(username,password,n_settimana,idMagazzino)VALUES('$nome','$hashed_password',' 1 ',' $idMagazzino ')";
+                        if ($connessione->query($sql3)) { //se non ci sono errori
+                            //recupero l'id dell'utente appena creato
+                            $sql4 = "SELECT id FROM utente ORDER BY id DESC LIMIT 1";
+                            $result = $connessione->query($sql4);
+                            if (mysqli_num_rows($result)) { //se non ci sono errori
+                                $idUtente = $result->fetch_array()['id'];
+                                //creo i costi fissi che dovrà pagare l'utente appena creato 
+                                $sql5 = "INSERT INTO costoFisso(nome,prezzo,idUtente)VALUES('Gas', 50, '$idUtente'),('Luce', 450, '$idUtente'),('Affitto', 1700, '$idUtente');";
+                                if ($connessione->query($sql5)) { //se non ci sono errori
+                                    //recupero gli id di ogni prodotto del gioco 
+                                    $sql6 = "SELECT id FROM prodotto ORDER BY id";
+                                    $result = $connessione->query($sql6);
+                                    if (mysqli_num_rows($result)) { //se non ci sono errori
+                                        while ($idProdotto = $result->fetch_array()) {
+                                            //inserisco i prodotti nel magazzino dell'utente con quantità = 0
+                                            $sql7 = "INSERT INTO immagazzina(idMagazzino,idProdotto,quantitàPr) VALUES ('$idMagazzino', '$idProdotto[id]', 0)";
+                                            if (!$connessione->query($sql7)) {
+                                                throw new Exception("Errore durante la insert nella tabella immagazzina per inserire i prodotti nel magazzino");
+                                            }
+                                        }
+                                    } else {
+                                        throw new Exception("Errore nella select dalla tabella prodotto per recuperare gli id");
+                                    }
+                                } else {
+                                    throw new Exception("Errore durante la insert nella tabella costoFisso");
+                                }
+                            } else {
+                                throw new Exception("Errore nella select dalla tabella utente per recuperare l'ultimo id");
+                            }
+                        } else {
+                            throw new Exception("Errore durante la insert nella tabella utente per creare un nuovo utente");
+                        }
+                    } else {
+                        throw new Exception("Errore nella select dalla tabella magazzino per recuperare l'ultimo id");
+                    }
+                } else {
+                    throw new Exception("Errore durante la insert nella tabella magazzino per creare un nuovo magazzino");
+                }
+            }
+        }
+    } catch (Exception $e) {
+        $_SESSION['error'] = 3; //errore con una query
+        $_SESSION['loggato'] = false; //blocco la registrazione
+        $connessione->close();
+    }
+
+    if (!isset($_SESSION['error'])) {
+        //registrazione avvenuta con successo
+        $_SESSION['loggato'] = true; //confermo la registrazione
+        header("location: ../private/home.php");
+    }
+}
 ?>
 
 <!DOCTYPE html>
@@ -31,6 +113,7 @@ require_once("../connessione.php");
                     <div class="mb-3">
                         <?php
                         if (isset($_SESSION['error'])) {
+                            $error = $_SESSION['error'];
                             echo "<div id='alert'>";
                         } else {
                             echo "<div id='alert' class='d-none'>";
@@ -48,27 +131,23 @@ require_once("../connessione.php");
                             <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Danger:">
                                 <use xlink:href="#exclamation-triangle-fill"></use>
                             </svg>
-                            <div>
-                                <strong id="testoAlert">
-                                    <?php
-                                    if (isset($_SESSION['error'])) {
-                                        $testo = "ERRORE";
-                                        $error = $_SESSION['error'];
-                                        switch ($error) {
-                                            case 1:
-                                                echo "Nome utente non valido";
-                                                break;
-                                            case 2:
-                                                echo "La password deve essere di almeno sei caratteri";
-                                                break;
-                                            case 3:
-                                                echo "Errore nella registrazione";
-                                                break;
-                                        }
-                                    }
-                                    ?>
-                                </strong>
-                            </div>
+                            <strong id="testoAlert">
+                                <?php
+                                switch ($error) {
+                                    case 1:
+                                        echo "Nome utente non valido";
+                                        break;
+                                    case 2:
+                                        echo "La password deve essere di almeno sei caratteri";
+                                        break;
+                                    case 3:
+                                        echo "Errore nella registrazione";
+                                        break;
+                                    default:
+                                        break;
+                                }
+                                ?>
+                            </strong>
                             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                             <!-- bottone per chiudere l'alert -->
                         </div>
@@ -92,100 +171,9 @@ require_once("../connessione.php");
                         <a href="./login.html">Log in</a>
                     </div>
                 </div>
+            </form>
         </div>
-        </form>
-    </div>
     </div>
 </body>
 
 </html>
-
-
-
-
-
-<?php
-$nome = $connessione->real_escape_string($_POST['nomeUtente']); //recupero il nome con cui si vuole effettuare la registrazione
-$password = $connessione->real_escape_string($_POST['password']); //recupero la password con cui si vuole effettuare la registrazione
-$hashed_password = password_hash($password, PASSWORD_DEFAULT); //eseguo una funzione di hashing sulla password
-
-
-try {
-    //verifico che il nome utente non sia già presente nel database
-    $sql = "SELECT username FROM utente WHERE username = '$nome'";
-    $result = $connessione->query($sql);
-    if (mysqli_num_rows($result)) {
-        $_SESSION['error'] = 1; //nome utente già presente nel database
-        $_SESSION['loggato'] = false; //blocco la registrazione
-        $connessione->close();
-        header("location: ./registrazione.php");
-    } else { //se il nome utente non è presente nel database vado avanti con la registrazione
-        if (strlen($password) < 6) { //se la password contiene meno di 6 caratteri blocco la registrazione
-            $_SESSION['error'] = 2; //password troppo corta
-            $_SESSION['loggato'] = false; //blocco la registrazione
-            $connessione->close();
-            //header("location: ./registrazione.html");
-        } else { //se la password ha più di 5 caratteri vado avanti con la registrazione
-            //creo il magazzino del nuovo utente
-            $sql1 = "INSERT INTO magazzino(dimensione)VALUES(20)";
-            if ($connessione->query($sql1)) { //se non ci sono errori
-                //recupero l'id del magazzino appena creato
-                $sql2 = "SELECT id FROM magazzino ORDER BY id DESC LIMIT 1";
-                $result = $connessione->query($sql2);
-                if (mysqli_num_rows($result)) { //se non ci sono errori
-                    $idMagazzino = $result->fetch_array()['id'];
-                    //creo il nuovo utente
-                    $sql3 = "INSERT INTO utente(username,password,n_settimana,idMagazzino)VALUES('$nome','$hashed_password',' 1 ',' $idMagazzino ')";
-                    if ($connessione->query($sql3)) { //se non ci sono errori
-                        //recupero l'id dell'utente appena creato
-                        $sql4 = "SELECT id FROM utente ORDER BY id DESC LIMIT 1";
-                        $result = $connessione->query($sql4);
-                        if (mysqli_num_rows($result)) { //se non ci sono errori
-                            $idUtente = $result->fetch_array()['id'];
-                            //creo i costi fissi che dovrà pagare l'utente appena creato 
-                            $sql5 = "INSERT INTO costoFisso(nome,prezzo,idUtente)VALUES('Gas', 50, '$idUtente'),('Luce', 450, '$idUtente'),('Affitto', 1700, '$idUtente');";
-                            if ($connessione->query($sql5)) { //se non ci sono errori
-                                //recupero gli id di ogni prodotto del gioco 
-                                $sql6 = "SELECT id FROM prodotto ORDER BY id";
-                                $result = $connessione->query($sql6);
-                                if (mysqli_num_rows($result)) { //se non ci sono errori
-                                    while ($idProdotto = $result->fetch_array()) {
-                                        //inserisco i prodotti nel magazzino dell'utente con quantità = 0
-                                        $sql7 = "INSERT INTO immagazzina(idMagazzino,idProdotto,quantitàPr) VALUES ('$idMagazzino', '$idProdotto[id]', 0)";
-                                        if (!$connessione->query($sql7)) {
-                                            throw new Exception("Errore durante la insert nella tabella immagazzina per inserire i prodotti nel magazzino");
-                                        }
-                                    }
-                                } else {
-                                    throw new Exception("Errore nella select dalla tabella prodotto per recuperare gli id");
-                                }
-                            } else {
-                                throw new Exception("Errore durante la insert nella tabella costoFisso");
-                            }
-                        } else {
-                            throw new Exception("Errore nella select dalla tabella utente per recuperare l'ultimo id");
-                        }
-                    } else {
-                        throw new Exception("Errore durante la insert nella tabella utente per creare un nuovo utente");
-                    }
-                } else {
-                    throw new Exception("Errore nella select dalla tabella magazzino per recuperare l'ultimo id");
-                }
-            } else {
-                throw new Exception("Errore durante la insert nella tabella magazzino per creare un nuovo magazzino");
-            }
-        }
-    }
-} catch (Exception $e) {
-    $_SESSION['error'] = 3; //errore con una query
-    $_SESSION['loggato'] = false; //blocco la registrazione
-    $connessione->close();
-    //header("location: ./registrazione.html");
-}
-
-if (!isset($_SESSION['error'])) {
-    //registrazione avvenuta con successo
-    $_SESSION['loggato'] = true; //confermo la registrazione
-    header("location: ../private/home.php");
-}
-?>
