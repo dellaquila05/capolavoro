cf7db047855f879b61f090cb7c5773545100788c
MomoS216 <sametmohamed@itis-molinari.eu>
2024-02-22 11:13:25 +0100

    aggiornamento

--- service/servizi.php
+++ service/servizi.php
@@ -1,25 +1,24 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-<meta charset="UTF-8">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <title>Home</title>
-<style>
-    table img {
-        width: 40%; /* Imposta la larghezza al 100% della larghezza del contenitore */
-        height: auto; /* Imposta l'altezza automaticamente per mantenere l'aspetto originale */
-    }
-</style>
+    <style>
+        table img {
+            width: 40%;
+            height: auto;
+        }
+    </style>
 </head>
 
 <body>
-
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
-            <a class="navbar-brand" href="./home.html">HomeTech</a>
+            <a class="navbar-brand" href="/home/private/home.html">HomeTech</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
                 aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -28,7 +27,7 @@
             <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#exampleModal">
                 <span class="material-symbols-outlined">
                     info
-                    </span>
+                </span>
             </button>
 
             <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -40,51 +39,122 @@
                         </div>
                         <div class="modal-body">
                             Benvenuto in HomeTech, il simulatore di gestione aziendale degli elettrodomestici!
-
-Inizia con un budget di 2000€ e gestisci il tuo impero settimana dopo settimana. Evita la bancarotta coprendo i costi fissi ogni 4 settimane. Affronta eventi imprevisti come guerre e furti, che influenzano le tue finanze.
-
-Ogni 48 settimane, paga le tasse e migliora la sicurezza con servizi come telecamere o allarmi. Acquista prodotti dal fornitore, soddisfa gli ordini e pianifica con attenzione. Sii strategico per trasformare il tuo impero in una storia di successo in HomeTech!
+                            Inizia con un budget di 2000€ e gestisci il tuo impero settimana dopo settimana. Evita la bancarotta coprendo i costi fissi ogni 4 settimane. Affronta eventi imprevisti come guerre e furti, che influenzano le tue finanze.
+                            Ogni 48 settimane, paga le tasse e migliora la sicurezza con servizi come telecamere o allarmi. Acquista prodotti dal fornitore, soddisfa gli ordini e pianifica con attenzione. Sii strategico per trasformare il tuo impero in una storia di successo in HomeTech!
                         </div>
                         <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Chiudi</button>
                         </div>
                     </div>
                 </div>
             </div>
-<div id="settimana">N settimana</div>
-        <div id="capitale">2000$</div>
+            <div id="settimana">N settimana</div>
+            <div id="capitale">2000$</div>
         </div>
     </nav>
-    <br><br><br>
 
+    <nav class="navbar navbar-dark bg-dark">
+  <a class="navbar-brand mx-auto" href="#">SERVIZI</a>
+</nav>
+
+    <br><br><br>
 
     <div class="container mt-4">
         <table class="table">
             <tbody>
                 <tr>
-                    <td><a href="/management/magazzino.php">Magazzino <br><img src="/home/img/magazzino.png" alt=""><br></a></td>
-                    <td><a href="/management/fornitore.php">Fornitore <br><img src="/home/img/fornitore.png" alt=""><br></a></td>
-                
+                    <td>
+                        <button type="button" class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#exampleModa2">Aggiungi telecamera</button>
+
+                        <div class="modal fade" id="exampleModa2" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h1 class="modal-title fs-5" id="exampleModalLabel">Aggiungi telecamera</h1>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        Aggiungi Telecamere al tuo spazio per soli €300 di installazione e €20,00 al mese. Riduci il rischio di furto del 14%. Sicurezza accessibile, tranquillità garantita.
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annulla</button>
+                                        <button type="button" class="btn btn-primary">Aggiungi</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </td>
+
+                    <td>
+                        <button type="button" class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#exampleModa3">Aumenta magazzino</button>
+
+                        <div class="modal fade" id="exampleModa3" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h1 class="modal-title fs-5" id="exampleModalLabel">Aumenta magazzino</h1>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        Aumenta la capacità del tuo magazzino del 100% con il nostro servizio dedicato. Ottimizza lo spazio e migliora l'efficienza operativa. Possiede un costo di 1000€ per migliorarlo e aumenta di 200€ l’affitto, oltre a ciò assumi un dipendente che ti costa 1700€ al mese
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annulla</button>
+                                        <button type="button" class="btn btn-primary">Aggiungi</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </td>
                 </tr>
                 <tr>
-                    <td><a href="/service/servizi.php" ><img src="/home/img/servizi.png" alt=""><br>Servizi</a></td>
-                    <td><a href="/service/resoconto.php"><img src="/home/img/resoconto.png" alt=""><br>Resoconto</a></td>
- 
+                    <td>
+                        <button type="button" class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#exampleModa4">Aggiungi allarme</button>
+
+                        <div class="modal fade" id="exampleModa4" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h1 class="modal-title fs-5" id="exampleModalLabel">Aggiungi allarme</h1>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        Aggiungi un Allarme al tuo ambiente con un'installazione di soli €199 e una fee mensile fissa di €14,99. Riduci il rischio di intrusioni del 29%. Sicurezza senza compromessi, per una tranquillità completa.
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annulla</button>
+                                        <button type="button" class="btn btn-primary">Aggiungi</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </td>
+
+                    <td>
+                        <button type="button" class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#exampleModa5">Aggiungi guardia</button>
+
+                        <div class="modal fade" id="exampleModa5" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h1 class="modal-title fs-5" id="exampleModalLabel">Aggiungi guardia</h1>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        Assicura la sicurezza del tuo magazzino con una guardia reale. Il costo mensile, che include stipendio e mantenimento, è solo €1.500. Riduci il rischio del 43%. Sicurezza garantita.
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annulla</button>
+                                        <button type="button" class="btn btn-primary">Aggiungi</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </td>
                 </tr>
             </tbody>
         </table>
-        <br><br>
-        <div class="row justify-content-end">
-            <div class="col-2">
-                <button type="submit" class="btn btn-primary">Avanti</button>
-            </div>
-        </div>
     </div>
-    </div>
-
-
-
-
-
 </body>
 </html>
+
