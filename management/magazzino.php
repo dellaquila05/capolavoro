2adc3e31ccc43cfb3e5510649e364c8eb3cf571c
Diego Dell'aquila <dellaquiladiego@itis-moliari.eu>
2024-03-15 10:13:35 +0100

    st

--- management/magazzino.php
+++ management/magazzino.php
@@ -16,7 +16,7 @@ session_start();
 <body>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
-            <a class="navbar-brand" href="../home/private/home.html">HomeTech</a>
+        <a class="navbar-brand" href="../home/private/home.php">HomeTech</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
